059f4cc98244ededda99f85fa7d5c02e9cec4db3
Jakub Kozdrowicz <jakub.kozdrowicz@gmail.com>
2018-09-11 19:36:59 +0200

    Migrated base apps. Set homepath.

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
+class Blog(models.Model):
+    title = models.CharField(max_length=200)
+    pubdate = models.DateTimeField()
+    body = models.TextField()
+    image = models.ImageField(upload_to='images')
+
+
+
 # Create your models here.
