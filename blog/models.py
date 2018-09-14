5821857056bcb3481b3fd9ed28268062966aec36
Jakub Kozdrowicz <jakub.kozdrowicz@gmail.com>
2018-09-14 16:35:22 +0200

    Detailed blog subpagess and webpage finish
    Overall webpage finish

--- blog/models.py
+++ blog/models.py
@@ -6,6 +6,16 @@ class Blog(models.Model):
     body = models.TextField()
     image = models.ImageField(upload_to='images')
 
+    #Ta funkcja pokazuje tytuł postu na stronie admina. Zawsze używaj ___str___ żeby wrzucić coś w górę do adminów.
+    def __str__(self):
+        return self.title
 
+    #Ta funkcja ogranicza długość tekstu na stronie wpisów, żeby nie było zbyt rozlegle i każdy mógł sobie wybrać...
+    def summary(self):
+        return self.body[:300]
+
+    #Funkcja formatująca datę w ludzki sposób...
+    def pubdate_short(self):
+        return self.pubdate.strftime('%a %d %b %Y')
 
 # Create your models here.
--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 
 from .models import Blog
 
@@ -6,4 +6,8 @@ def allblogs(request):
     blogs = Blog.objects
     return render(request, 'blog/allblogs.html', {'blogs':blogs})
 
+#Ta funkcja kieruje na podstronę wybranego wpisu na blogu.
+def detail(request, blog_id):
+    detailblog = get_object_or_404(Blog, pk=blog_id)
+    return render(request, 'blog/detail.html', {'blog':detailblog})
 # Create your views here.
