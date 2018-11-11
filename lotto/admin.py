5e0aa7bd6985082f291529658a0a80e0a91dde09
Jakub Kozdrowicz <jakub.kozdrowicz@gmail.com>
2018-11-11 17:27:04 +0100

    Migrating randomizer to main DB - Models
    Created and registered models as I'm migrating my program to my main postgres db under Django for the sake of simplicity and order.

--- lotto/admin.py
+++ lotto/admin.py
@@ -1,6 +1,10 @@
 from django.contrib import admin
 
-from .models import Lotto
+from .models import Lotto, Game1, Game2, Game3, Game4
 
 admin.site.register(Lotto)
+admin.site.register(Game1)
+admin.site.register(Game2)
+admin.site.register(Game3)
+admin.site.register(Game4)
 # Register your models here.
