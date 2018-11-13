39424d2914524a55a25e51c5b241601045ba5090
Jakub Kozdrowicz <jakub.kozdrowicz@gmail.com>
2018-11-13 23:02:15 +0100

    SQL for db4 in randomizer
    Request seems to work fine in development for db4. More tests tomorrow.

--- lotto/updatedb.py
+++ lotto/updatedb.py
@@ -29,9 +29,6 @@ html = urllib.request.urlopen(url, context=ctx).read()
 soup = BeautifulSoup(html, 'html.parser')
 lsource = str(soup.find_all('td'))
 g1 = re.findall(">(\d\d\d\d)</td>, <td>(\d\d)[.](\d\d)[.](\d\d\d\d)</td>, <td>(\d\d) (\d\d) (\d\d) (\d\d) (\d\d) [+] <b>(\d\d)<", lsource)
-g1 = g1[0]
-print(g1)
-#Na starcie łączy się z siecią i aktualizuje bazy danych dla wszystkich 4 gier losowych.
 def connect(var):
     conn=psycopg2.connect("dbname=webappbasedb user=postgres password=Ma3taksamo_Jakja")
     cur=conn.cursor()
@@ -48,9 +45,9 @@ def connect(var):
         df1= df.drop(df.columns[1],1)
         df1.to_sql('game3', conn, if_exists='replace', index=False)
     elif var == 4 :
-    #    cur.execute('CREATE TABLE IF NOT EXISTS game4 ("1" INTEGER, "2" INTEGER, "3" INTEGER, "4" INTEGER, "5" INTEGER, "6" INTEGER, "7" INTEGER, "8" INTEGER, "9" INTEGER, "10" INTEGER)')
-        cur.execute('INSERT INTO game4 ("Id","year","month","day","N1","N2","N3","N4","N5","R1") VALUES (g1[0],g1[1],g1[2],g1[3],g1[4],g1[5],g1[6],g1[7],g1[8],g1[9]) ON CONFLICT ("Id") DO NOTHING')
-        cur.execute('CREATE UNIQUE INDEX idx_casenum ON game4 ("Id")')
+        cur.execute('CREATE TABLE IF NOT EXISTS game4 ("1" INTEGER UNIQUE, "2" INTEGER, "3" INTEGER, "4" INTEGER, "5" INTEGER, "6" INTEGER, "7" INTEGER, "8" INTEGER, "9" INTEGER, "10" text)')
+        cur.execute('CREATE UNIQUE INDEX IF NOT EXISTS idx_casenum ON game4 ("1")')
+        cur.executemany('INSERT INTO game4 ("1","2","3","4","5","6","7","8","9","10") VALUES (%s,%s,%s,%s,%s,%s,%s,%s,%s,%s) ON CONFLICT ("1") DO NOTHING', [(v) for v in g1])
     conn.commit()
     conn.close()
 
