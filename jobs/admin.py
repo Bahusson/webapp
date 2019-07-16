3a18a0a595788ab9b4024bf23084700acedba614
Jakub <j.kozdrowicz@apdeveloper.com.pl>
2019-07-16 17:07:24 +0200

    Tłumaczenie i Jinja 2 działa na Home
    Pozostaje poprawić na pozostałych stronach.

--- jobs/admin.py
+++ jobs/admin.py
@@ -1,9 +1,8 @@
 from django.contrib import admin
 
-from .models import Trick, Tech
+from .models import Trick, Tech, Pageitem
 
-
+admin.site.register(Pageitem)
 admin.site.register(Trick)
-
 admin.site.register(Tech)
 # Register your models here.
