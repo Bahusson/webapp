fa71c960aef18a90e5088219944a8ef32d810cf2
Jakub <40240192+Bahusson@users.noreply.github.com>
2019-07-19 11:41:03 +0200

    Poprawienie klasy Database dla Randomizera

--- myprograms/randomize1.py
+++ myprograms/randomize1.py
@@ -6,33 +6,45 @@ from bokeh.models import HoverTool, ColumnDataSource
 import re
 import configparser
 import datetime
+from special.snippets import bug_catch
 
-    class Database:
 
-        def __init__(self, db):
-            conn=psycopg2.connect(db)
-            cur=conn.cursor()
+class Database(object):
 
-        def selector(self,base,table,day1,month1,year1,day2,month2,year2,q):
-            if q == 1:
-                duck is '<=' and quack is ">="
-            elif q == 0:
-                duck and quack is '='
-            if base == 4 or 8:
-                range = "1"
-                execall = 'SELECT "2", "3", "4", "5", "6", "7", "8", "9", "10" FROM %s LIMIT %s OFFSET %s', (table, rowto, rowfrom)
-            else:
-                range = "0"
-                execall = 'SELECT * FROM %s LIMIT %s OFFSET %s', (table,rowto,rowfrom)
-            cur.execute('SELECT MIN(%s) FROM %s WHERE "2" %s %s AND "3"=%s AND "4"=%s',(range,table,duck,day2,month2,year2))
-            rowfrom=(cur.fetchone()[0])
-            cur.execute('SELECT MAX(%s) FROM %s WHERE "2" %s %s AND "3"=%s AND "4"=%s',(range,table,quack,day1,month1,year1))
-            rowfrom=(cur.fetchone()[0])-
-            cur.execute(execall)
-            rows=cur.fetchall()
-            conn.close()
+    def __init__(self, db):
+        self.conn = psycopg2.connect(db)
+        self.cur = self.conn.cursor()
 
-#Główna oś funkcji
+    def selector(
+         self, base, table, day1, month1, year1, day2, month2, year2, q, ):
+        if q == 1:
+            duck = '<='
+            quack = ">="
+        elif q == 0:
+            duck = '='
+            quack = '='
+        if base == 4 or 8:
+            range = "1"
+            execall = 'SELECT "2", "3", "4", "5", "6", "7", "8", "9", "10" FROM %s LIMIT %s OFFSET %s', (
+             table, rowto, rowfrom, )
+        else:
+            range = "0"
+            execall = 'SELECT * FROM %s LIMIT %s OFFSET %s', (
+             table, rowto, rowfrom, )
+        self.cur.execute(
+         'SELECT MIN(%s) FROM %s WHERE "2" %s %s AND "3"=%s AND "4"=%s', (
+          range, table, duck, day2, month2, year2, ))
+        self.rowfrom = (self.cur.fetchone()[0])
+        self.cur.execute(
+         'SELECT MAX(%s) FROM %s WHERE "2" %s %s AND "3"=%s AND "4"=%s', (
+          range, table, quack, day1, month1, year1, ))
+        self.rowfrom = (self.cur.fetchone()[0])
+        self.cur.execute(execall)
+        self.rows = self.cur.fetchall()
+        self.conn.close()
+
+
+# Główna oś funkcji
 def generate(request):
     global radio
     global datal
@@ -47,8 +59,10 @@ def generate(request):
     global table
     if request.is_ajax():
         radio = request.POST.get('gamesel')
-        datfr = re.findall("(\d\d\d\d)-(\d\d)-(\d\d)", request.POST['datefrom'])
-        datto = re.findall("(\d\d\d\d)-(\d\d)-(\d\d)", request.POST['dateto'])
+        datfr = re.findall(r"(\d\d\d\d)-(\d\d)-(\d\d)",
+                           request.POST['datefrom'])
+        datto = re.findall(r"(\d\d\d\d)-(\d\d)-(\d\d)",
+                           request.POST['dateto'])
         datal = request.POST['dateall']
         nhilo = request.POST['numhilow']
         norol = request.POST['norolls']
@@ -74,77 +88,80 @@ def generate(request):
     else:
         pass
 
-    enumerators(rad,moftn,nhilo)
-    makedf(rad,avsco,grgen)
+    enumerators(rad, moftn, nhilo, )
+    makedf(rad, avsco, grgen, )
 
     if norol == '1':
         rolls = "Nie wybrano losowań"
     elif norol == "0" and datal == "0":
         rollhead = ["Losowania wraz z datą:" + "\n"]
         rowspacing = list()
-        while len(rowspacing)<len(rows):
+        while len(rowspacing) < len(rows):
             rowspacing.append("\n")
-        zippedrows = zip(rows, rowspacing)
+        zippedrows = zip(rows, rowspacing, )
         ziprows = list(zippedrows)
-        rolls = [rollhead, ziprows]
+        rolls = [rollhead, ziprows, ]
     elif norol == "0" and datal == "1":
         rolls = "Zaznaczono całość pomiarów"
     else:
         bug_catch()
 
     responsedata = {
-        'hilow' : hilow,
-        'rolls' : rolls,
-        'often' : often,
-        'avgsc' : avgsc,
-#Co do ostatniego to możliwe, że trzeba będzie dodaĆ warunkowe return/render HttpResponse pod ten graf.
-        'graph' : graph
+        'hilow': hilow,
+        'rolls': rolls,
+        'often': often,
+        'avgsc': avgsc,
+        # Co do ostatniego to możliwe, że trzeba będzie dodaĆ
+        # warunkowe return/render HttpResponse pod ten graf.
+        'graph': graph
     }
     return JsonResponse(responsedata)
 
-#Moje nowe narzędzie do łapania errorów w kodzie. Jest prawdziwie piękne. :)
-def bug_catch():
-    try:
-        raise ValueError('Represents a hidden bug, do not catch this')
-        raise Exception('This is the exception you expect to handle')
-    except Exception as error:
-        print('Caught this error: ' + repr(error))
-#Jeśli ma choĆ w połowie tyle skuteczności co piękna na wejrzeniu, to będę używał... <3
 
-#Można uży tej funkcji do uporządkowania spraw z backendem tak żeby go nie przepisywa w całości.
-#To fukcja przypisująca numery od 1 do 8 w zależności od przypadku pazy danych i tego czy
+# Można uży tej funkcji do uporządkowania spraw
+# z backendem tak żeby go nie przepisywać w całości.
+# To fukcja przypisująca numery od 1 do 8 w zależności od
+# przypadku bazy danych i tego czy
 # jest filtrowana czy nie.
 def radparam():
     global rad
-    if datal + radio == "11" :
+    if datal + radio == "11":
         rad = 1
-    elif datal + radio == "12" :
+    elif datal + radio == "12":
         rad = 2
-    elif datal + radio == "13" :
+    elif datal + radio == "13":
         rad = 3
-    elif datal + radio == "14" :
+    elif datal + radio == "14":
         rad = 4
-    elif datal + radio == "01" :
+    elif datal + radio == "01":
         rad = 5
-        searchA(1,dattoo[2],dattoo[1],dattoo[0],datfro[2],datfro[1],datfro[0])
-    elif datal + radio == "02" :
+        searchA(
+                1, dattoo[2], dattoo[1], dattoo[0],
+                datfro[2], datfro[1], datfro[0], )
+    elif datal + radio == "02":
         rad = 6
-        searchA(2,dattoo[2],dattoo[1],dattoo[0],datfro[2],datfro[1],datfro[0])
-    elif datal + radio == "03" :
+        searchA(
+                2, dattoo[2], dattoo[1], dattoo[0],
+                datfro[2], datfro[1], datfro[0], )
+    elif datal + radio == "03":
         rad = 7
-        searchA(3,dattoo[2],dattoo[1],dattoo[0],datfro[2],datfro[1],datfro[0])
-    elif datal + radio == "04" :
+        searchA(
+                3, dattoo[2], dattoo[1], dattoo[0],
+                datfro[2], datfro[1], datfro[0], )
+    elif datal + radio == "04":
         rad = 8
-        searchA(4,dattoo[2],dattoo[1],dattoo[0],datfro[2],datfro[1],datfro[0])
+        searchA(
+                4, dattoo[2], dattoo[1], dattoo[0],
+                datfro[2], datfro[1], datfro[0], )
 
     else:
         pass
 
 
-#Ta funkcja ogranicza bazę rakordów do konkretnych dat.
-def searchA(base,day1,month1,year1,day2,month2,year2):
-    conn=psycopg2.connect("dbname=webappbasedb user=postgres password=Ma3taksamo_Jakja")
-    cur=conn.cursor()
+# Ta funkcja ogranicza bazę rakordów do konkretnych dat.
+def searchA(base, day1, month1, year1, day2, month2, year2, ):
+    conn = psycopg2.connect("dbname=webappbasedb user=postgres password=Ma3taksamo_Jakja")
+    cur = conn.cursor()
     global rowfrom
     global rowto
     global rows
---
+++ special/snippets.py
@@ -0,0 +1,7 @@
+# Bugcatcher snippet
+def bug_catch():
+    try:
+        raise ValueError('Represents a hidden bug, do not catch this')
+        raise Exception('This is the exception you expect to handle')
+    except Exception as error:
+        print('Caught this error: ' + repr(error))
